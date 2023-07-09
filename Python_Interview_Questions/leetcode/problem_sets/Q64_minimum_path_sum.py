17607034ab272d72daa1598adbd16a980cd339d0
jacson-chong <jacson.chong@bigpayme.com>
2023-07-10 00:14:55 +0800

    Solve Leetcode Q64 Minimum Path Sum

---
+++ Python_Interview_Questions/leetcode/problem_sets/Q64_minimum_path_sum.py
@@ -0,0 +1,44 @@
+from typing import List
+
+def minPathSum(grid: List[List[int]]) -> int:
+    """
+    Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes the sum of all numbers along its path.
+
+    Note: You can only move either down or right at any point in time.
+
+    Example 1:
+    Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
+    Output: 7
+    Explanation: Because the path 1 → 3 → 1 → 1 → 1 minimizes the sum.
+
+    Example 2:
+    Input: grid = [[1,2,3],[4,5,6]]
+    Output: 12
+    
+    Constraints:
+    • m == grid.length
+    • n == grid[i].length
+    • 1 <= m, n <= 200
+    • 0 <= grid[i][j] <= 200
+    """
+    m = len(grid)
+    n = len(grid[0])
+    assert 1 <= m <= 200, "Length of grid must be between 1 and 200"
+    assert 1 <= n <= 200, "Length of grid must be between 1 and 200"
+
+    for i in range(m):
+        for j in range(n):
+            assert 0 <= grid[i][j] <= 200, "Number in grid must be between 0 and 200"
+
+    result = grid
+    for rowIdx, _ in enumerate(grid):
+        for colIdx in range(len(grid[rowIdx])):
+            if rowIdx == 0 and colIdx == 0:
+                continue
+            elif rowIdx == 0 and colIdx > 0:
+                result[rowIdx][colIdx] += result[rowIdx][colIdx - 1]
+            elif rowIdx != 0 and colIdx == 0:
+                result[rowIdx][colIdx] += result[rowIdx - 1][colIdx]
+            else:
+                result[rowIdx][colIdx] += min(result[rowIdx - 1][colIdx], result[rowIdx][colIdx - 1])
+    return result[-1][-1]
---
+++ Python_Interview_Questions/leetcode/tests/Q64_minimum_path_sum_test.py
@@ -0,0 +1,9 @@
+import pytest
+from leetcode.problem_sets.Q64_minimum_path_sum import minPathSum
+
+def test_minPathSum():
+    grid = [[1,3,1],[1,5,1],[4,2,1]]
+    assert minPathSum(grid) == 7
+
+    grid = [[1,2,3],[4,5,6]]
+    assert minPathSum(grid) == 12
